0c069706d9f6e0e29e0c32cbacab5ddcbcad9f63
coder-codes <60198123+coder-codes@users.noreply.github.com>
2020-01-24 21:01:02 +0530

    Add files via upload

---
+++ credit_card_val.py
@@ -0,0 +1,15 @@
+#function for ccredit card validation 
+#algorithem used luhn algorithem
+def credit_card_vali(card_number):
+    #check_number = int(card_number)%10
+    sum = 0
+    if  not(len(card_number)):
+        return 0
+    else :
+        for digit in range(0,len(card_number)-1):
+            sum += int(card_number[digit])
+        if int(card_number[-1]) +(sum%10== 0):
+            return 1 
+        else:
+            return 0
+
